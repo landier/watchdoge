f5b1799124c03592f7ebfe5b879b38da5ee60190
Nicolas Landier <nicolas.landier@gmail.com>
2019-05-03 23:45:31 +0200

    [front] -> TypeScript + Jumbotron page

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<p>Hello World</p>, document.getElementById('boum'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

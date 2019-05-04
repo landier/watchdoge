40e24c767be6a4425e964dd748fb5d48d18a2eea
Nicolas Landier <nicolas.landier@gmail.com>
2019-05-04 22:00:42 +0200

    front - add notification

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import 'bootstrap/js/dist/alert';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<p>Hello World</p>, document.getElementById('boum'));
+ReactDOM.render(
+    <div>
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+            <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+        </div>
+        <p>Hello World</p>
+    </div>, document.getElementById('boum'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

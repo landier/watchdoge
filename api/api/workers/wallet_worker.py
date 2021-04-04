f673f8cd1d411dddd4cea5e2affecdec89da4e6e
Nicolas Landier <nicolas.landier@gmail.com>
2021-04-04 03:50:43 +0200

    Fix debug main in WalletWorker

--- api/api/workers/wallet_worker.py
+++ api/api/workers/wallet_worker.py
@@ -2,8 +2,14 @@ import asyncio
 from api.models.asset import Asset
 import asyncio
 from icecream import ic
+import binance
+import os
 
-WALLET_WORKER_PERIOD_IN_S = 60
+
+BINANCE_API_KEY=os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET=os.getenv("BINANCE_API_SECRET")
+
+WALLET_SYNC_PERIOD = int(os.getenv("WATCHDODGE_WALLET_SYNC_PERIOD", 60))
 
 
 class WalletWorker():
@@ -18,16 +24,19 @@ class WalletWorker():
         "Fetch assets"
         while not self.shutdown:
             ic("bim " + str(self.name))
-            await asyncio.sleep(1)
+            await asyncio.sleep(WALLET_SYNC_PERIOD)
+
 
 async def main():
     ww1 = WalletWorker(1)
     asyncio.create_task(ww1.fetch_assets())
 
+
 if __name__ == '__main__':
+    ww = WalletWorker(1)
+
     loop = asyncio.get_event_loop()
-    # asyncio.run(main())
-    loop.run_until_complete(main)
-    
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(
+    tasks = list()
+    tasks.append(loop.create_task(ww.fetch_assets()))
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()

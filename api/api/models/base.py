52f3c2e796608c3e0bf6a46ca7700ff76ee473c3
Nicolas Landier <nicolas.landier@gmail.com>
2021-04-05 22:10:13 +0200

    Enable SQLAlchemy logs

--- api/api/models/base.py
+++ api/api/models/base.py
@@ -8,7 +8,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./watchdodge.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False},
-    # echo=True
+    echo=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

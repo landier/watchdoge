5626ef421db5c8ff9f6f28efeed1d7de85664795
Nicolas Landier <nicolas.landier@gmail.com>
2021-05-31 21:14:58 +0200

    Add ping endpoint

--- api/api/app.py
+++ api/api/app.py
@@ -103,3 +103,7 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+@app.get("/ping/")
+def ping():
+    return {'response': 'pong'}
